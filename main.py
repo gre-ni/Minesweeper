2b4fd4acf7d8b058e7a2f6d037778c3da056e839
NikolG <greplova.nikol@gmail.com>
2026-01-30 21:04:12 +0100

    Layout small adjustments

--- main.py
+++ main.py
@@ -15,9 +15,22 @@ top_frame = Frame(
     root, # Location
     bg="#202020",
     width=1440,
-    height=180
+    height=180,
+    highlightbackground="#525252",
+    highlightthickness=1
 )
 top_frame.place(x=0,y=0)
+
+# Frames:
+left_frame = Frame(
+    root, # Location
+    bg="#202020",
+    width=360,
+    height=541,
+    highlightbackground="#525252",
+    highlightthickness=1
+)
+left_frame.place(x=0,y=179)
 
 # Run the window:
 root.mainloop() # It will run until I close it
